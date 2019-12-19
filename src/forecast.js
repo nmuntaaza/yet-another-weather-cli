c3ff521b8459273a6812f09b108f785a64bbfa2b
Naufal <nmuntaaza.wh@gmail.com>
2019-12-19 17:47:55 +0700

    add handling for axios, change app version

--- src/forecast.js
+++ src/forecast.js
@@ -1,6 +1,7 @@
 import Conf from 'conf';
 import Table from 'cli-table3';
 import { configKey} from './configure';
+import httpStatus from 'http-status';
 import {
   validateApiKey,
   validateCityName,
@@ -20,31 +21,45 @@ export async function forecast(args) {
   const [unitsValidation, unitsValidationText] = validateUnits(units);
 
   if (apiKeyValidation && cityNameValidation && unitsValidation) {
-    const { data } = await queryWeatherForecast(cityName, units, apiKey);
-    const tempUnits = (units == 'metric') ? '˚C' : '˚F';
-    const table = new Table({
-      head: ['City', 'DateTime', 'Weather', `Temp(${tempUnits})`],
-      colWidths: [25, 23, 18, 12],
-      wordWrap: true
-    });
+    try {
+      const { data } = await queryWeatherForecast(cityName, units, apiKey);
+      const tempUnits = (units == 'metric') ? '˚C' : '˚F';
+      const table = new Table({
+        head: ['City', 'DateTime', 'Weather', `Temp(${tempUnits})`],
+        colWidths: [25, 23, 18, 12],
+        wordWrap: true
+      });
 
-    const forecastLimit = args.limit ? args.limit : 5;
-    data.list.slice(0, forecastLimit).forEach(forecast => {
-      table.push([
-        cityName,
-        new Date(forecast.dt * 1000).toLocaleString(),
-        forecast.weather[0].description,
-        forecast.main.temp
-      ]);
-    })
+      const forecastLimit = args.limit ? args.limit : 5;
+      data.list.slice(0, forecastLimit).forEach(forecast => {
+        table.push([
+          cityName,
+          new Date(forecast.dt * 1000).toLocaleString(),
+          forecast.weather[0].description,
+          forecast.main.temp
+        ]);
+      })
 
-    console.log(table.toString());
-    return;
+      console.log(table.toString());
+      return;
+    } catch (err) {
+      if (err.isAxiosError) {
+        if (err.response.status == httpStatus.NOT_FOUND) {
+          console.error(`${chalk.redBright('\n  City is not found\n')}`);
+        }
+        if (err.response.status == httpStatus.UNAUTHORIZED) {
+          console.error(`${chalk.redBright('\n  apiKey is invalid probably expired. Unauthorized\n')}`);
+        }
+      } else {
+        console.error(`${chalk.redBright('\n  Error is happened\n')}`);
+      } 
+    }
+  } else {
+    let errMessage = `${chalk.redBright('\n  This option value is not valid:\n')}`;
+    if (!apiKeyValidation) errMessage += `${chalk.redBright('  --apiKey')}: ${apiKeyValidationText}\n`;
+    if (!cityNameValidation) errMessage += `${chalk.redBright('  --cityName')}: ${cityNameValidationText}\n`;
+    if (!unitsValidation) errMessage += `${chalk.redBright('  --units')}: ${unitsValidationText}\n`;
+    console.error(errMessage);
   }
-
-  let errMessage = `${chalk.redBright('\n  This option value is not valid:\n')}`;
-  if (!apiKeyValidation) errMessage += `${chalk.redBright('  --apiKey')}: ${apiKeyValidationText}\n`;
-  if (!cityNameValidation) errMessage += `${chalk.redBright('  --cityName')}: ${cityNameValidationText}\n`;
-  if (!unitsValidation) errMessage += `${chalk.redBright('  --units')}: ${unitsValidationText}\n`;
-  console.error(errMessage);
+  return;
 }
--- src/now.js
+++ src/now.js
@@ -8,6 +8,7 @@ import {
   queryCurrentWeather
 } from './utils';
 import chalk from 'chalk';
+import httpStatus from 'http-status';
 
 export async function now(args) {
   const config = new Conf().get(configKey) || {};
@@ -20,28 +21,41 @@ export async function now(args) {
   const [unitsValidation, unitsValidationText] = validateUnits(units);
 
   if (apiKeyValidation && cityNameValidation && unitsValidation) {
-    const { data } = await queryCurrentWeather(cityName, units, apiKey);
-    const tempUnits = (units == 'metric') ? '˚C' : '˚F';
-    const table = new Table({
-      head: ['City', 'DateTime', 'Weather', `Temp(${tempUnits})`],
-      colWidths: [25, 23, 18, 12],
-      wordWrap: true
-    });
+    try {
+      const { data } = await queryCurrentWeather(cityName, units, apiKey);
+      const tempUnits = (units == 'metric') ? '˚C' : '˚F';
+      const table = new Table({
+        head: ['City', 'DateTime', 'Weather', `Temp(${tempUnits})`],
+        colWidths: [25, 23, 18, 12],
+        wordWrap: true
+      });
 
-    table.push([
-      data.name,
-      new Date(data.dt * 1000).toLocaleString(),
-      data.weather[0].description,
-      data.main.temp
-    ]);
+      table.push([
+        data.name,
+        new Date(data.dt * 1000).toLocaleString(),
+        data.weather[0].description,
+        data.main.temp
+      ]);
 
-    console.log(table.toString());
-    return;
+      console.log(table.toString());
+    } catch (err) {
+      if (err.isAxiosError) {
+        if (err.response.status == httpStatus.NOT_FOUND) {
+          console.error(`${chalk.redBright('\n  City is not found\n')}`);
+        }
+        if (err.response.status == httpStatus.UNAUTHORIZED) {
+          console.error(`${chalk.redBright('\n  apiKey is invalid probably expired. Unauthorized\n')}`);
+        }
+      } else {
+        console.error(`${chalk.redBright('\n  Error is happened\n')}`);
+      }
+    }
+  } else {
+    let errMessage = `${chalk.redBright('\n  This option value is not valid:\n')}`;
+    if (!apiKeyValidation) errMessage += `${chalk.redBright('  --apiKey')}: ${apiKeyValidationText}\n`;
+    if (!cityNameValidation) errMessage += `${chalk.redBright('  --cityName')}: ${cityNameValidationText}\n`;
+    if (!unitsValidation) errMessage += `${chalk.redBright('  --units')}: ${unitsValidationText}\n`;
+    console.error(errMessage);
   }
-
-  let errMessage = `${chalk.redBright('\n  This option value is not valid:\n')}`;
-  if (!apiKeyValidation) errMessage += `${chalk.redBright('  --apiKey')}: ${apiKeyValidationText}\n`;
-  if (!cityNameValidation) errMessage += `${chalk.redBright('  --cityName')}: ${cityNameValidationText}\n`;
-  if (!unitsValidation) errMessage += `${chalk.redBright('  --units')}: ${unitsValidationText}\n`;
-  console.error(errMessage);
+  return;
 }
