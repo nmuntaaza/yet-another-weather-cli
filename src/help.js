35499965cad382d65b3ec04db4abc5bec3f226f3
Naufal <nmuntaaza.wh@gmail.com>
2019-12-23 22:16:46 +0700

    Update help description

--- src/help.js
+++ src/help.js
@@ -4,55 +4,59 @@ const menus = {
   main: `
   ${chalk.blueBright('weather [command] <options>')}
 
-    ${chalk.blueBright('now')} ................ show weather for now
-    ${chalk.blueBright('forecast')} ........... show weather forecast
-    ${chalk.blueBright('config')}.............. set API key, city name, and temperature units
-    ${chalk.blueBright('version')} ............ show package version
+    ${chalk.blueBright('add')} ................ set your places for later use
+    ${chalk.blueBright('config')} ............. set API key and units
+    ${chalk.blueBright('forecast')} ........... show forecast for the text three hour
+    ${chalk.blueBright('now')} ................ show current weather
+    ${chalk.blueBright('version')} ............ get app version
   `,
-  now: `
-  ${chalk.blueBright('usage: weather now <options>')}
+  add: `
+  ${chalk.blueBright('usage: weather add [--placeName] [--cityName]')}
 
-  options:
-    --cityName: what city you want to know
-    --units: what is the units you want, metric for 'celcius' and 'imperial' for fahrenheit
-    --apiKey: your apiKey from https://openweathermap.org/. Sign in to get your apiKey
-  
-  if no option is passed, it will read from config file that you set.
-  
-  See also: weather help config.
+  parameter:
+    --placeName: your place name (eg. Home) ${chalk.blueBright('Required')}
+    --cityName: where city your place is (eg. London) ${chalk.blueBright('Required')}
+  `,
+  config: `
+  ${chalk.blueBright('usage: weather config [--apiKey] [--units]')}
+
+  parameter:
+    --apiKey: apiKey from https://openweathermap.org/. Sign in to get your apiKey ${chalk.blueBright('Required')}
+    --units: is the units you want, metric for 'celcius' and 'imperial' for fahrenheit ${chalk.blueBright('Optional')}
+
+  if you dont set units it will using default 'metric'
+  this command will save your setting to config file in your account folder
+  if there is no value in the option, it will use you setted value
+
+  See also: weather now, weather forecast.
   `,
   forecast: `
-  ${chalk.blueBright('usage: weather forecast <options>')}
+  ${chalk.blueBright('usage: weather forecast [--cityName] [--limit] [--units]')}
 
-  options:
-    --cityName: city you want to know the weather
-    --units: is the units you want, metric for 'celcius' and 'imperial' for fahrenheit
-    --apiKey: apiKey from https://openweathermap.org/. Sign in to get your apiKey
-    --limit: limit for how much data will you get
+  parameter:
+    --cityName: city you want to know the weather ${chalk.blueBright('Required')}
+    --limit: limit for how much data will you get ${chalk.blueBright('Optional')}
+    --units: is the units you want, metric for 'celcius' and 'imperial' for fahrenheit ${chalk.blueBright('Optional')}
 
   if no option is passed, it will read from config file that you set.
 
+  See also: weather help config.
+  `,
+  now: `
+  ${chalk.blueBright('usage: weather now [--cityName] [--units]')}
+
+  parameter:
+    --cityName: city name you want to know the weather (eg. London) ${chalk.blueBright('Optional')}
+    --units: what is the units you want, metric for 'celcius' and 'imperial' for fahrenheit ${chalk.blueBright('Optional')}
+  
+  if no parameter is passed, it will read from config file that you set
+  
   See also: weather help config.
   `,
   version: `
   ${chalk.blueBright('usage: weather forecast <options>')}
 
   show weather cli version app
-  `,
-  config: `
-  ${chalk.blueBright('usage: weather config <options>')}
-
-  options:
-    --cityName: city you want to know the weather
-    --units: is the units you want, metric for 'celcius' and 'imperial' for fahrenheit
-        metric will be used if not defined
-    --apiKey: apiKey from https://openweathermap.org/. Sign in to get your apiKey
-
-  this command will save your setting to config file in your account folder
-  when calling 'weather now <options>' if there is no value in the option,
-  it will use you setted value.
-
-  See also: weather now, weather forecast.
   `
 }
 
